96570463c4cafb0640e3cfba7368a549c80264c3
Vinicius <vinicius@lambdatt.com.br>
2026-02-04 09:48:21 -0300

    FindByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,6 +15,12 @@ public class Program {
 		System.out.println("| Teste 1 - FindById |");	
 		Seller seller = sellerDao.findById(3);	
 		System.out.println(seller);
+		
+		System.out.println("| Teste 2 - FindByDepartment |");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);	
+		
+		for (Seller sel : list) System.out.println(sel);		
 		
 
 	}
